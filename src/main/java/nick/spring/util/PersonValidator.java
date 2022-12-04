e7cbe5008599acc7f1467d8e62c270cd36d86481
Nick <nikitaverhovod@gmail.com>
2022-12-05 02:21:49 +0300

    forgotten

---
+++ src/main/java/nick/spring/util/PersonValidator.java
@@ -0,0 +1,34 @@
+package nick.spring.util;
+
+
+import nick.spring.models.Person;
+import nick.spring.services.PeopleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+
+@Component
+public class PersonValidator implements Validator {
+
+    private final PeopleService peopleService;
+
+    @Autowired
+    public PersonValidator(PeopleService peopleService) {
+        this.peopleService = peopleService;
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return Person.class.equals(aClass);
+    }
+
+    @Override
+    public void validate(Object o, Errors errors) {
+        Person person = (Person) o;
+
+        if (peopleService.getPersonByName(person.getName()).isPresent())
+            errors.rejectValue("name", "", "Человек с таким ФИО уже существует");
+    }
+}
